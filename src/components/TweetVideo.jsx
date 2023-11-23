a550688e48b69d695c861f5a2fcd9781fe128cfe
DonK <nathanaeldon495@gmail.com>
2023-11-23 18:21:08 +0100

    Video management

--- src/components/TweetVideo.jsx
+++ src/components/TweetVideo.jsx
@@ -10,7 +10,7 @@ import Video from '../images/Discipline_avec_Coby.mp4'
 
 function VideoPlayer() {
     return (
-      <video src={Video} controls>
+      <video src={Video} controls ="autoplay loop muted " >
         Votre navigateur ne prend pas en charge la lecture de vidéos.
       </video>
     );
