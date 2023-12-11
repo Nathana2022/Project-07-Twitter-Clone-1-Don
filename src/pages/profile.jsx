7aed2767fd4cd0bac58d2fb1c5ca50c91b3ccc4e
Nathana2022 <nathanaeldon495@gmail.com>
2023-12-11 03:16:40 +0100

    Style : Ajout du style sur la page profile

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,44 +1,44 @@
 import React from "react";
+import BackIcon from "../components/BackIcon";
+import ProfileActionsIcons from "../components/profileActionsIcons";
 
 export default function Profile() {
   return (
     <main>
-      <div>
-        <h1>Don Nathanael</h1>
-        <p>560</p>
-        <button></button>
-      </div>
-      <div>
-        <img src="" alt="" />
-      </div>
-      <div>
-        <img src="" alt="" />
-        <button></button>
-      </div>
-      <div>
-        <div>
-          <p>Don Nathanael</p>
-          <p>@donnathanael</p>
+      <div className="profile-header">
+        <div className="ph-icon">
+          <BackIcon />
         </div>
-        <div>
+        <div className="ph-title">
+          <h1>Don Nathanael</h1>
+          <p>560</p>
+        </div>
+      </div>
+      <div className="profile-cover">
+        <img src="/cover.jpeg" alt="" />
+      </div>
+      <div className="profile-avatar-section">
+        <img src="/profile.jpg" alt="" />
+        <a href="#" className="profile-edit-button">Edit profile</a>
+      </div>
+      <div className="profile-content">
+        <div className="pc-names">
+          <p className="pc-title">Don Nathanael</p>
+          <p className="pc-username">@donnathanael</p>
+        </div>
+        <div className="pc-infos">
           <div>
-            <span>icon</span>
+            <ProfileActionsIcons icon="map" />
             <span>Democratic Republic of Congo</span>
           </div>
           <div>
-            <span>icon</span>
+            <ProfileActionsIcons icon="calandar" />
             <span>Joined January 2023</span>
           </div>
         </div>
-        <div>
-          <div>
-            <span>66</span>
-            <span>Following</span>
-          </div>
-          <div>
-            <span>1</span>
-            <span>Follower</span>
-          </div>
+        <div className="pc-foll">
+          <a href="#"><span className="bold">66</span> Following</a>
+          <a href="#"><span className="bold">1</span> Follower</a>
         </div>
       </div>
     </main>
